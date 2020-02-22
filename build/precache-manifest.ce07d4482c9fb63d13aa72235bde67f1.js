f4aa3d31eef723c79e303f29fc7c9ec8a6e64659
Gavin <ekkambyte@gmail.com>
2020-02-22 09:56:03 -0500

    added front end and data

---
+++ build/precache-manifest.ce07d4482c9fb63d13aa72235bde67f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee3d4b067e26c938da089118b425655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd6bac3399923e691f9e",
+    "url": "/static/js/2.48b98873.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.48b98873.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d6c6b0a0de8538a275",
+    "url": "/static/js/main.41449644.chunk.js"
+  },
+  {
+    "revision": "4f8b734a7e80109b6f7e",
+    "url": "/static/js/runtime-main.10a268e8.js"
+  }
+]);
